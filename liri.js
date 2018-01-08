84094ab2e736c1fddee775c03dea3338f32be83a
Liz Adams <thelza12345@Elizabeths-MacBook-Pro-2.local>
2018-01-08 13:33:59 -0500

    liri-node-app

--- liri.js
+++ liri.js
@@ -1,29 +1,49 @@
 require("dotenv").config();
 
+
 var spotify = new Spotify(keys.spotify);
-  var client = new Twitter(keys.twitter);
+var client = new Twitter(keys.twitter);
 
-//   * `my-tweets`
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: <47504b85f3554897b549829bfb2edc9d>,
+  secret: <12bb62cb7ebe44af8076ba6fbcd2d7ea>
+});
+ 
 
-// * `spotify-this-song`
+function getSong() {
+	var 
+spotify.search({ type: 'track', query: 'The Sign' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data);
 
-// * `movie-this`
+});
+
+
+
+function getTweets(){
+
+}
+
+function getSong() {
+
+}
+
+function getMovie() {
+
+}
 
 // * `do-what-it-says`
 
 var request = require("request");
 
-// Run the request function...
-// The request function takes in a URL then returns three arguments:
-// 1. It provides an error if one exists.
-// 2. It provides a response (usually that the request was successful)
-// 3. It provides the actual body text from the website <---- what actually matters.
 request( "http://www.omdbapi.com/?i=tt3896198&apikey=d3783854", function(error, response, body) {
 
-  // If the request was successful...
   if (!error && response.statusCode === 200) {
-
-    // Then log the body from the site!
     console.log(body);
   }
 });
